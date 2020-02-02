c8bfdf4601fd6479d793dc6c6192c18899847e98
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-02-02 00:18:30 -0500

    finished age calculator

---
+++ JavaScript_Introduction/ageCalculator.js
@@ -0,0 +1,7 @@
+var userAge = prompt("What's your age?");
+var firstName = prompt("What's your first name?");
+var lastName = prompt("What's your last name?");
+var fullName = firstName + " " + lastName;
+var daysOld = userAge + 1 * 365;
+alert("Hey, " + fullName + "! You are "+ daysOld + " days old. Congratulations on your life." );
+console.log(daysOld);
