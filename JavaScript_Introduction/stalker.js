e81a2a2202c38a5e59c2e38b771c6de07c69e198
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-02-01 21:39:00 -0500

    finished stalker and practice

---
+++ JavaScript_Introduction/stalker.js
@@ -0,0 +1,5 @@
+let firstName = prompt("What is your first name?");
+let lastName = prompt('What is your last name?');
+let usersAge = prompt('What is your age?');
+alert("Welcome "+ firstName + " " + lastName + ". Who is " + usersAge + " years old.");
+console.log("Welcome "+ firstName + " " + lastName + ". Who is " + usersAge + " years old.");
