a87172df3289c81cf16ef6ed941c9e0db812bbc5
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-02-13 22:57:26 -0500

    added changes to winner display and hovering changes to buttons

--- DOM_Manipulation/scoreKeeper.js
+++ DOM_Manipulation/scoreKeeper.js
@@ -1,3 +1,5 @@
+
+
 //GamePlay
 var gameOver = false;
 var gameFinishScore = document.getElementById("gameFinishScore");
@@ -9,6 +11,7 @@ var winningScore = playTillNumberValue;
 
 
 //Player 1
+var player1Title = document.getElementById("player1");
 var htmlPlayer1Score = document.getElementById("player1Score");
 var player1Score = 0;
 var player1Button = document.getElementById("player1Button");
@@ -16,12 +19,13 @@ var player1Button = document.getElementById("player1Button");
 player1Button.addEventListener('click', function (){
   if(!gameOver){
     player1Score++;
+    player1Button.classList.add("btn-outline-danger");
     console.log(player1Score);
     if(playTillNumber.value == player1Score){
       gameOver = true;
       htmlPlayer1Score.classList.add("winner");
       if(htmlPlayer1Score.className === "winner")
-      console.log("Player 1, You have won!!!");
+      player1Title.innerText = "Player 1, You have won!!!";
     }
     htmlPlayer1Score.innerText = player1Score;
   }
@@ -29,6 +33,7 @@ player1Button.addEventListener('click', function (){
 });
 
 //Player 2
+var player2Title = document.getElementById("player2");
 var htmlPlayer2Score = document.getElementById("player2Score");
 var player2Score = 0;
 var player2Button = document.getElementById("player2Button");
@@ -36,12 +41,13 @@ var player2Button = document.getElementById("player2Button");
 player2Button.addEventListener("click", function(){
   if(!gameOver){
     player2Score++;
+    player2Button.classList.add("btn-outline-primary");
     console.log(player2Score);
     if(playTillNumber.value == player2Score){
       htmlPlayer2Score.classList.add("winner");
       gameOver = true;
       if(htmlPlayer2Score.className === "winner")
-      console.log("Player 2, You have won!!!");
+      player2Title.innerText = "Player 2, You have won!!!";
     }
     htmlPlayer2Score.innerText = player2Score;
   }
@@ -67,7 +73,19 @@ resetButton.addEventListener("click", function(){
 });
 
 
+// //buttons
+// var buttons = docutment.querySelector("button);
 
+//   buttons.addEventListener("mouseover", function(){
+//     for(var i = 0; i<buttons.length; i++){
+//       this.classList.add("hovering");
+//   });
+  
+//   buttons.addEventListener("mouseout", function(){
+//     for(var t = 0; t<buttons.length; t++){
+//       this.classList.remove("hovering");
+//     }
+//   });
 
 
 
