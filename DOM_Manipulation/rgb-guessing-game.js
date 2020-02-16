551e437ed482090a3d4ec411c1f3a2599d6f9da0
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-02-16 14:51:47 -0500

    created random cardcolors

--- DOM_Manipulation/rgb-guessing-game.js
+++ DOM_Manipulation/rgb-guessing-game.js
@@ -1,72 +1,91 @@
-//create for random numbers
+//Creating random RGB numbers from 1 - 250
 
 var min = 1;
 var max = 250;
-var capture = [0, 0, 0];
-var correctRGBNum = []
-
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
+//-------------------------------------------------------------------------------
 
-for(var a = 0; a < capture.length; a++){
-  correctRGBNum.splice(capture[a], 0, getRandomIntInclusive(min, max))
-  console.log(correctRGBNum);
+//Create correct RGB combination
+var correctRGB = []
+
+for(var i =0; i<3; i++){
+  correctRGB.push(getRandomIntInclusive(min, max))
+  console.log(correctRGB);
 }
 
+//--------------------------------------------------------------------------------
 
-var correctRed = correctRGBNum[0];
-var correctGreen = correctRGBNum[1];
-var correctBlue = correctRGBNum[2];
-document.getElementById("rgb1").innerText = correctRed;
-document.getElementById("rgb2").innerText = correctGreen;
-document.getElementById("rgb3").innerText = correctBlue;
+//Creating random RGB combinations
 
-//Change colors of cards
-var cardMin = 0;
-var cardMax = 5;
-var capture2 = [0, 0, 0, 0, 0, 0];
-var cardPosition = [];
-var winningPosition = "The winning position of the RGB color is at index ";
-
-function cardNumberSelected(cardMin, cardMax) {
-  cardMin = Math.ceil(cardMin);
-  cardMax = Math.floor(cardMax);
-  return Math.floor(Math.random() * (cardMax - cardMin + 1)) + cardMin; //The maximum is inclusive and the minimum is inclusive
-}
-
-for(var b = 0; b <=1000; b++){
-  let cardSelected = cardNumberSelected(cardMin, cardMax);
-  console.log(cardSelected);
-
-  if(cardPosition.length !== 6 && cardPosition.includes(cardSelected) === false){
-    cardPosition.push(cardSelected);
-  }
-  console.log(cardPosition);
-}
-
-for(var c = 0; c < cardPosition.length; c++){
-  if(cardPosition[c] === 1){
-    console.log(winningPosition + c);
-    cardPosition[c] = correctRGBNum;
-  }
-}
-
-//attaching random colors to boxes
-
-var colorBox1 = cardPosition[0];
-var colorBox2 = cardPosition[1];
-var colorBox3 = cardPosition[2];
-var colorBox4 = cardPosition[3];
-var colorBox5 = cardPosition[4];
-var colorBox6 = cardPosition[5];
 
 var colorMixedArray = [];
+var createdRGB2 = [];
+var createdRGB3 = [];
+var createdRGB4 = [];
+var createdRGB5 = [];
+var createdRGB6 = [];
+
 
 for(var d =0; d < 15; d++){
-    colorMixedArray.splice(colorMixedArray[d], 0, getRandomIntInclusive(min, max))
+    colorMixedArray.push(getRandomIntInclusive(min, max));
     console.log(colorMixedArray);
+  
+  if(createdRGB2.length < 3){
+    createdRGB2.push(colorMixedArray[0]);
+    colorMixedArray.shift();
+    console.log(createdRGB2);
+  } else if(createdRGB3.length < 3){
+    createdRGB3.push(colorMixedArray[0]);
+    colorMixedArray.shift();
+    console.log(createdRGB3)
+  } else if(createdRGB4.length < 3){
+    createdRGB4.push(colorMixedArray[0]);
+    colorMixedArray.shift(createdRGB4);
+  } else if(createdRGB5.length < 3){
+    createdRGB5.push(colorMixedArray[0]);
+    colorMixedArray.shift(createdRGB5);
+  }else if(createdRGB6.length < 3){
+    createdRGB6.push(colorMixedArray[0]);
+    colorMixedArray.shift(createdRGB6);
+  
+  }
 }
+
+    colorMixedArray.push(createdRGB2);
+    colorMixedArray.push(createdRGB3);
+    colorMixedArray.push(createdRGB4);
+    colorMixedArray.push(createdRGB5);
+    colorMixedArray.push(createdRGB6);
+    colorMixedArray.push(correctRGB);
+
+    // shuffling colorMixedArray and finding correctRGB array index
+
+let shufflingArray = function(arr){
+  let newPosition;
+  let temporaryIndex;
+    //This is a backwards loop
+  for(let e = arr.length -1; e >0; e--){
+    newPosition = Math.floor(Math.random()*(e+1));
+    temporaryIndex = arr[e];
+    arr[e] = arr[newPosition];
+    arr[newPosition] = temporaryIndex;
+  }
+  return arr;
+}
+//-------------------
+
+var shuffledColorArray = shufflingArray(colorMixedArray);
+console.log("this is the shuffledColorArray - ");  
+console.log(shuffledColorArray);
+
+  console.log("This is the colorMixedArray - " + colorMixedArray);
+  console.log("THis is the original cardPosition array - "  + cardPosition);
+  console.log("This is the correctRGB array - " + correctRGB);
+  
+  console.log("The index of correctRGB is ");
+  shuffledColorArray.indexOf(correctRGB);
