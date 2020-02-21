512f925efc82632e5beb336f1fdcb78158e2971a
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-02-21 18:45:17 -0500

    created eventListener to each color div

--- DOM_Manipulation/rgbGuessingGame.js
+++ DOM_Manipulation/rgbGuessingGame.js
@@ -15,7 +15,12 @@ colorDisplay.textContent = pickedColor;
 for(var i = 0; i < squares.length; i++){
   squares[i].style.backgroundColor = colors[i];
   squares[i].addEventListener("click", function(){
-    
+    var clickedColor = this.style.backgroundColor;
+    if(clickedColor === pickedColor){
+      alert("Correct");
+    } else {
+      alert("try again")
+    }
   });
 
 }
