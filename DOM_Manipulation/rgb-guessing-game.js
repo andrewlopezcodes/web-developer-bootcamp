289795be11c4b37e1a65d06075d8a8ade88eb5d6
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-02-21 07:33:02 -0500

    fixed createRandomRGBCombinationsForHardSquares function. We have random 6 rgb created. going to attach them to squares

--- DOM_Manipulation/rgb-guessing-game.js
+++ DOM_Manipulation/rgb-guessing-game.js
@@ -3,7 +3,7 @@
 var min = 1;
 var max = 250;
 var numberOfRandomNumbersNeededForRGB;
-var numberOfRandomNumbersNeededForSquares ;
+var numberOfRandomNumbersNeededForSquares = 15;
 
 var correctRGB = [];
 var menuRGB1 = document.getElementById("menuRGB1");
@@ -19,6 +19,7 @@ var createdRGBhard4 = [];
 var createdRGBhard5 = [];
 var createdRGBhard6 = [];
 var finishedShuffledColorArray;
+var intakeArray;
 
 
 function getRandomIntInclusive(min, max) {
@@ -48,42 +49,41 @@ menuRGB3.innerText = correctRGB[2];
 
 //Creating random RGB combinations
 
-function createRandomRGBCombinationsForSquares (){
-  numberOfRandomNumbersNeededForSquares = 15;
-  if(numberOfRandomNumbersNeededForSquares > 6){
-    for(var d =0; d < numberOfRandomNumbersNeededForSquares; d++){
-      colorMixedArray.push(getRandomIntInclusive(min, max));
-      console.log(hardColorMixedArray);
-    }  
-  } else {
-    for(var e = 0; e < numberOfRandomNumbersNeededForSquares; e++){
-      easyColorMixedArray.push(getRandomIntInclusive(min, max));
-      console.log(easyColorMixedArray)
-    }
-  }
-}
-
-
-
-
-  
-  if(createdRGBhard2.length < 3 && numberOfRandomNumbersNeededForSquares > 6){
+function fillingHardColorMixedArray(){
+  if(createdRGBhard2.length < 3){
     createdRGBhard2.push(hardColorMixedArray[0]);
     hardColorMixedArray.shift();
-  } else if(createdRGBhard3.length < 3 && numberOfRandomNumbersNeededForSquares > 6){
+    console.log("This is the createdRGBhard2 array - " + createdRGBhard2);
+  } else if(createdRGBhard3.length < 3 ){
     createdRGBhard3.push(hardColorMixedArray[0]);
     hardColorMixedArray.shift();
-  } else if(createdRGBhard4.length < 3 && numberOfRandomNumbersNeededForSquares > 6){
+  } else if(createdRGBhard4.length < 3 ){
     createdRGBhard4.push(hardColorMixedArray[0]);
-    hardColorMixedArray.shift();
-  } else if(createdRGBhard5.length < 3 && numberOfRandomNumbersNeededForSquares > 6){
+    hardColorMixedArray.shift(createdRGBhard4);
+  } else if(createdRGBhard5.length < 3 ){
     createdRGBhard5.push(hardColorMixedArray[0]);
-    hardColorMixedArray.shift();
-  }else if(createdRGBhard6.length < 3 && numberOfRandomNumbersNeededForSquares > 6){
+    hardColorMixedArray.shift(createdRGBhard5);
+  }else if(createdRGBhard6.length < 3 ){
     createdRGBhard6.push(hardColorMixedArray[0]);
-    hardColorMixedArray.shift();
+    hardColorMixedArray.shift(createdRGBhard6);
+    
   
+  } else {
+    console.log("help")
+  } 
+};
+  
+
+function createRandomRGBCombinationsForHardSquares (){
+for(var d = 0; d < numberOfRandomNumbersNeededForSquares; d++){
+    hardColorMixedArray.push(getRandomIntInclusive(min, max));
+    fillingHardColorMixedArray(hardColorMixedArray);
   }
+  
+}
+
+createRandomRGBCombinationsForHardSquares();
+
 
     hardColorMixedArray.push(createdRGBhard2);
     hardColorMixedArray.push(createdRGBhard3);
@@ -92,11 +92,16 @@ function createRandomRGBCombinationsForSquares (){
     hardColorMixedArray.push(createdRGBhard6);
     hardColorMixedArray.push(correctRGB);
 
+    
+ 
+
+
     // shuffling colorMixedArray and finding correctRGB array index
 
 let shufflingArray = function(arr){
   let newPosition;
   let temporaryIndex;
+
     //This is a backwards loop
   for(let e = arr.length -1; e >0; e--){
     newPosition = Math.floor(Math.random()*(e+1));
@@ -105,42 +110,97 @@ let shufflingArray = function(arr){
     arr[newPosition] = temporaryIndex;
   }
   return arr;
-}
+};
+
+//____________________
+// for(var d =0; d < 15; d++){
+//   colorMixedArray.push(getRandomIntInclusive(min, max));
+//   console.log(colorMixedArray);
+
+// if(createdRGB2.length < 3){
+//   createdRGB2.push(colorMixedArray[0]);
+//   colorMixedArray.shift();
+//   console.log(createdRGB2);
+// } else if(createdRGB3.length < 3){
+//   createdRGB3.push(colorMixedArray[0]);
+//   colorMixedArray.shift();
+//   console.log(createdRGB3)
+// } else if(createdRGB4.length < 3){
+//   createdRGB4.push(colorMixedArray[0]);
+//   colorMixedArray.shift(createdRGB4);
+// } else if(createdRGB5.length < 3){
+//   createdRGB5.push(colorMixedArray[0]);
+//   colorMixedArray.shift(createdRGB5);
+// }else if(createdRGB6.length < 3){
+//   createdRGB6.push(colorMixedArray[0]);
+//   colorMixedArray.shift(createdRGB6);
+
+// }
+// }
+
+// colorMixedArray.push(createdRGB2);
+// colorMixedArray.push(createdRGB3);
+// colorMixedArray.push(createdRGB4);
+// colorMixedArray.push(createdRGB5);
+// colorMixedArray.push(createdRGB6);
+// colorMixedArray.push(correctRGB);
+
+// // shuffling colorMixedArray and finding correctRGB array index
+// let shufflingArray = function(arr){
+// let newPosition;
+// let temporaryIndex;
+//   //This is a backwards loop
+// for(let e = arr.length -1; e >0; e--){
+//   newPosition = Math.floor(Math.random()*(e+1));
+//   temporaryIndex = arr[e];
+//   arr[e] = arr[newPosition];
+//   arr[newPosition] = temporaryIndex;
+// }
+// return arr;
+// }
+
+// //-------------------
+
+// var shuffledColorArray = shufflingArray(colorMixedArray);
+// console.log("this is the shuffledColorArray - ");  
+// console.log(shuffledColorArray);
+
+// console.log("This is the colorMixedArray - " + colorMixedArray);
+// console.log("This is the correctRGB array - " + correctRGB);
+
+// console.log("The index of correctRGB is " + shuffledColorArray.indexOf(correctRGB));
+
 //-------------------
 
-function shuffledColorArray(arr){
-  if(arr === hardColorMixedArray){
-    return finishedShuffledColorArray = shufflingArray(arr);
-  } else if(arr === easyColorMixedArray){
-    return finishedShuffledColorArray = shufflingArray(arr);
-  }
-}
 
-console.log("this is the finishedShuffledColorArray - ");  
-console.log(finishedShuffledColorArray);
+
+var colorsReadyForAttachment = shufflingArray(hardColorMixedArray);
+
+console.log("this is the shufflingArray - ");  
+console.log(colorsReadyForAttachment);
 
   console.log("This is the hardColorMixedArray - " + hardColorMixedArray);
   console.log("This is the easyColorMixedArray - " + easyColorMixedArray);
   console.log("This is the correctRGB array - " + correctRGB);
   
-  console.log("The index of correctRGB is " + finishedShuffledColorArray.indexOf(correctRGB));
+  // console.log("The index of correctRGB is " + finishedShuffledColorArray.indexOf(correctRGB));
 
  //attaching colors to boxes
 
 
-  var firstColor = `rgb(` + shuffledColorArray[0].toString() + `)`;
-  var secondColor = `rgb(` + shuffledColorArray[1].toString() + `)`;
-  var thirdColor = `rgb(` + shuffledColorArray[2].toString() + `)`;
-  var fourthColor = `rgb(` + shuffledColorArray[3].toString() + `)`;
-  var fifthColor = `rgb(` + shuffledColorArray[4].toString() + `)`;
-  var sixthColor = `rgb(` + shuffledColorArray[5].toString() + `)`;
+  // var firstColor = `rgb(` + shuffledColorArray[0].toString() + `)`;
+  // var secondColor = `rgb(` + shuffledColorArray[1].toString() + `)`;
+  // var thirdColor = `rgb(` + shuffledColorArray[2].toString() + `)`;
+  // var fourthColor = `rgb(` + shuffledColorArray[3].toString() + `)`;
+  // var fifthColor = `rgb(` + shuffledColorArray[4].toString() + `)`;
+  // var sixthColor = `rgb(` + shuffledColorArray[5].toString() + `)`;
   
-  // var square1 = document.getElementById("colorBox1").style.backgroundColor = firstColor;
-  // var square2 = document.getElementById("colorBox2").style.backgroundColor = secondColor;
-  // var square3 = document.getElementById("colorBox3").style.backgroundColor = thirdColor;
-  // var square4 = document.getElementById("colorBox4").style.backgroundColor = fourthColor;
-  // var square5 = document.getElementById("colorBox5").style.backgroundColor = fifthColor;
-  // var square6 = document.getElementById("colorBox6").style.backgroundColor = sixthColor;
+  // // var square1 = document.getElementById("colorBox1").style.backgroundColor = firstColor;
+  // // var square2 = document.getElementById("colorBox2").style.backgroundColor = secondColor;
+  // // var square3 = document.getElementById("colorBox3").style.backgroundColor = thirdColor;
+  // // var square4 = document.getElementById("colorBox4").style.backgroundColor = fourthColor;
+  // // var square5 = document.getElementById("colorBox5").style.backgroundColor = fifthColor;
+  // // var square6 = document.getElementById("colorBox6").style.backgroundColor = sixthColor;
 
-  //EventListeners to make saures disapear
+  // //EventListeners to make saures disapear
   
